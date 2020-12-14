701787a3fa5986e4b84cafaceb508930f398b8cb
dedoblege <dedoblege@gmail.com>
2020-12-14 16:31:05 +0100

    update available cheats list

--- lambda/cheats.js
+++ lambda/cheats.js
@@ -6,5 +6,56 @@ module.exports = {
       "Derecha, Izquierda, R1, R1, R1, Izquierda, Triángulo, Triángulo, X, Círculo, L1, L1.",
     bicicleta:
       "Izquierda, Izquierda, Derecha, Derecha, Izquierda, Derecha, Cuadrado, Círculo, Triángulo, R1, R2.",
+    "camión de la basura":
+      "Círculo, R1, Círculo, R1, Izquierda, Izquierda, R1, L1, Círculo, Derecha.",
+    "carrito de golf": "Círculo, L1, Izquierda, R1, L2, X, R1, L1, Círculo, X.",
+    "coche de carreras":
+      "R2, L1, Círculo, Derecha, L1, R1, Derecha, Izquierda, Círculo, R2.",
+    "coche deportivo": "R1, Círculo, R2, Derecha, L1, L2, X, X, Cuadrado, R1.",
+    helicóptero:
+      "Círculo, Círculo, L1, Círculo, Círculo, Círculo, L1, L2, R1, Triángulo, Círculo, Triángulo.",
+    limusina:
+      "R2, Derecha, L2, Izquierda, Izquierda, R1, L1, Círculo, Derecha.",
+    "motocicleta de motocross":
+      "Círculo, X, L1, Círculo, Círculo, L1, Círculo, R1, R2, L2, L1, L1.",
+    motocicleta:
+      "R1, Derecha, Izquierda, Derecha, R2, Izquierda, Derecha, Cuadrado, Derecha, L2, L1, L1.",
+    paracaídas:
+      "Izquierda, Derecha, L1, L2, R1, R2, R2, Izquierda, Izquierda, Derecha, L1.",
+    "salud al máximo":
+      "Círculo, L1, Triángulo, R2, X, Cuadrado, Círculo, Derecha, Cuadrado, L1, L1, L1.",
+    invencibilidad:
+      "Derecha, X, Derecha, Izquierda, Derecha, R1, Derecha, Izquierda, X, Triángulo.",
+    "balas de fuego":
+      "L1, R1, Cuadrado, R1, Izquierda, R2, R1, Izquierda, Cuadrado, Derecha, L1, L1.",
+    "balas explosivas":
+      "Derecha, cuadrado, X, Izquierda, R1, R2, Izquierda, Derecha, Derecha, L1, L1, L1.",
+    "desbloquear todas las armas":
+      "Triángulo, R2, Izquierda, L1, X, Derecha, Triángulo, Abajo, Cuadrado, L1, L1, L1.",
+    "modo borracho":
+      "Triángulo, Derecha, Derecha, Izquierda, Derecha, cuadrado, Círculo, Izquierda.",
+    "correr rápido":
+      "Triángulo, Izquierda, Derecha, Derecha, L2, L1, cuadrado.",
+    "nadar rápido":
+      "Izquierda, Izquierda, L1, Derecha, Derecha, R2, Izquierda, L2, Derecha.",
+    "puñetazos explosivos":
+      "Derecha, Izquierda, X, Triángulo, R1, Círculo, Círculo, Círculo, L2.",
+    "reducir nivel de búsqueda":
+      "R1, R1, Círculo, R2, Derecha, Izquierda, Derecha, Izquierda, Derecha, Izquierda.",
+    "aumentar nivel de búsqueda":
+      "R1, R1, Círculo, R2, Izquierda, Derecha, Izquierda, Derecha, Izquierda, Derecha.",
+    "aumentar nivel de especial":
+      "X, X, cuadrado, R1, L1, X, Derecha, Izquierda, X.",
+    "apuntado a cámara lenta":
+      "Cuadrado, L2, R1, Triángulo, Izquierda, cuadrado, L2, Derecha, X.",
+    "coches deslizables": "Triángulo R1, R1, Izquierda, R1, L1, R2, L1.",
+    "cámara lenta": "Triángulo, Izquierda, Derecha, Derecha, cuadrado, R2, R1.",
+    "modificar el clima": "R2, X, L1, L1, L2, L2, L2, Cuadrado.",
+    "super salto":
+      "Izquierda, Izquierda, Triángulo, Triángulo, Derecha, Derecha, Izquierda, Derecha, Cuadrado, R1, R2.",
+    "caída libre":
+      "L1, L2, R1, R2, Izquierda, Derecha, Izquierda, Derecha, L1, L2, R1, R2, Izquierda, Derecha, Izquierda, Derecha.",
+    "gravedad lunar":
+      "Izquierda, Izquierda, L1, R1, L1, Derecha, Izquierda, L1, Izquierda.",
   },
 };
