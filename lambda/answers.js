db2033d12e69cc4edc291eaaeb976a4f649cea76
Delia <dedoblege@gmail.com>
2020-12-22 15:15:01 +0100

    Correction to answers to avoid saying "punto"

--- lambda/answers.js
+++ lambda/answers.js
@@ -6,16 +6,16 @@ module.exports = {
       "Puedes pedirme trucos, como por ejemplo, dime truco para %s.",
     DISPLAY_CARD_TITLE: "%s  - Truco para %s.",
     HELP_MESSAGE:
-      "Puedes pedirme trucos como, dime el truco para  %s, o, puedes decir salir...Ahora cómo puedo ayudarte?",
+      "Puedes pedirme trucos como, dime el truco para  %s, o, puedes decir salir..Ahora cómo puedo ayudarte?",
     HELP_REPROMPT:
-      "Puedes decir cosas como, dime el truco para  %s, o, puedes decir salir...Ahora cómo puedo ayudarte?",
+      "Puedes decir cosas como, dime el truco para  %s, o, puedes decir salir..Ahora cómo puedo ayudarte?",
     STOP_MESSAGE: "Hasta pronto!",
     CHEAT_REPEAT_MESSAGE: "Intenta decir repite.",
     CHEAT_NOT_FOUND_WITH_ITEM_NAME:
       "Perdona, ahora mismo no conozco el truco para ",
     CHEAT_NOT_FOUND_WITHOUT_ITEM_NAME:
       "Perdona, ahora mismo no conozco ese truco. ",
-    CHEAT_NOT_FOUND_REPROMPT: "...Con qué más puedo ayudarte?",
+    CHEAT_NOT_FOUND_REPROMPT: "..Con qué más puedo ayudarte?",
     ALEXA_HELP:
       "Puedes pedirme trucos de G T A! Qué truco quieres que te diga?",
     ALEXA_GOODBYE: "Hasta pronto!",
