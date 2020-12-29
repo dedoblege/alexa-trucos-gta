5bd54af715da3f9d7a5fa13f38f819769d6fb4b9
Josep Tormo <jotorcos@Icarus.local>
2020-12-29 12:58:29 +0100

    Add english answers in answers.js

--- lambda/answers.js
+++ lambda/answers.js
@@ -1,35 +1,51 @@
 module.exports = {
   ANSWERS_ES_ES: {
-    SKILL_NAME: "Trucos G T A",
-    WELCOME_MESSAGE: "Bienvenido a trucos G T A.; Qué truco quieres saber?",
+    SKILL_NAME: 'Trucos G T A',
+    WELCOME_MESSAGE: 'Bienvenido a trucos G T A.; Qué truco quieres saber?',
     WELCOME_REPROMPT:
-      "Puedes pedirme trucos, como por ejemplo, dime truco para %s.",
-    DISPLAY_CARD_TITLE: "%s  - Truco para %s.",
+      'Puedes pedirme trucos, como por ejemplo, dime truco para %s.',
+    DISPLAY_CARD_TITLE: '%s  - Truco para %s.',
     HELP_MESSAGE:
-      "Puedes pedirme trucos como, dime el truco para  %s, o, puedes decir salir..Ahora cómo puedo ayudarte?",
+      'Puedes pedirme trucos como, dime el truco para  %s, o, puedes decir salir. Ahora cómo puedo ayudarte?',
     HELP_REPROMPT:
-      "Puedes decir cosas como, dime el truco para  %s, o, puedes decir salir..Ahora cómo puedo ayudarte?",
-    STOP_MESSAGE: "Hasta pronto!",
-    CHEAT_REPEAT_MESSAGE: "Intenta decir repite.",
+      'Puedes decir cosas como, dime el truco para  %s, o, puedes decir salir. Ahora cómo puedo ayudarte?',
+    STOP_MESSAGE: 'Hasta pronto!',
+    CHEAT_REPEAT_MESSAGE: 'Intenta decir repite.',
     CHEAT_NOT_FOUND_WITH_ITEM_NAME:
-      "Perdona, ahora mismo no conozco el truco para ",
+      'Perdona, ahora mismo no conozco el truco para ',
     CHEAT_NOT_FOUND_WITHOUT_ITEM_NAME:
-      "Perdona, ahora mismo no conozco ese truco. ",
-    CHEAT_NOT_FOUND_REPROMPT: "..Con qué más puedo ayudarte?",
+      'Perdona, ahora mismo no conozco ese truco. ',
+    CHEAT_NOT_FOUND_REPROMPT: '..Con qué más puedo ayudarte?',
     ALEXA_HELP:
-      "Puedes pedirme trucos de G T A! Qué truco quieres que te diga?",
-    ALEXA_GOODBYE: "Hasta pronto!",
+      'Puedes pedirme trucos de G T A! Qué truco quieres que te diga?',
+    ALEXA_GOODBYE: 'Hasta pronto!',
     ALEXA_FALLBACK:
-      "Perdona, no sé que me estás pidiendo. Por favor, vuelve a intentarlo.",
+      'Perdona, no sé que me estás pidiendo. Por favor, vuelve a intentarlo.',
     ALEXA_ERROR:
-      "Perdona, ha ocurrido un error procesando tu petición. Por favor, vuelve a intentarlo.",
-    ALEXA_REFLECTOR: "Acabas de invocar",
+      'Perdona, ha ocurrido un error procesando tu petición. Por favor, vuelve a intentarlo.',
+    ALEXA_REFLECTOR: 'Acabas de invocar',
   },
   ANSWERS_EN_US: {
-    ALEXA_HELP: "You can say hello to me! How can I help?",
-    ALEXA_GOODBYE: "Goodbye!",
+    SKILL_NAME: 'G T A Cheats',
+    WELCOME_MESSAGE: 'Welcome to G T A Cheats. What cheat do you want to know?',
+    WELCOME_REPROMPT:
+      'You can ask about cheats, for example, tell me a cheat for %s.',
+    DISPLAY_CARD_TITLE: '%s  - Cheat for %s.',
+    HELP_MESSAGE:
+      'You can ask for cheats like, tell me the cheat %s, or, you can say exit. Now, how can I help you?',
+    HELP_REPROMPT:
+      'You can say things like, tell me the cheat %s, or, you can say exit. Now, how can I help you?',
+    STOP_MESSAGE: 'See you soon!',
+    CHEAT_REPEAT_MESSAGE: 'Try saying repeat.',
+    CHEAT_NOT_FOUND_WITH_ITEM_NAME:
+      "Sorry, right now I don't know the cheat for ",
+    CHEAT_NOT_FOUND_WITHOUT_ITEM_NAME:
+      "Sorry, right now I don't know that cheat. ",
+    CHEAT_NOT_FOUND_REPROMPT: 'How can I help you?',
+    ALEXA_HELP: 'You can ask for G T A cheats! What cheat do you want to know?',
+    ALEXA_GOODBYE: 'Goodbye!',
     ALEXA_FALLBACK: "Sorry, I don't know about that. Please try again.",
-    ALEXA_ERROR: "Sorry, I had trouble doing what you asked. Please try again.",
-    ALEXA_REFLECTOR: "You just triggered",
+    ALEXA_ERROR: 'Sorry, I had trouble doing what you asked. Please try again.',
+    ALEXA_REFLECTOR: 'You just triggered',
   },
-};
+}
